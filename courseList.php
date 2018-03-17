f712238764779b24914011d25f38aecc4ebf138d
yygatech <yygatech@gmail.com>
2018-03-17 13:33:27 -0500

    Create course.php

---
+++ courseList.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yygatech
+ * Date: 3/17/18
+ * Time: 1:18 PM
+ */
+?>
+
+<div id="card-deck">
+    <div class="card-deck m-2">
+
+        <?php include 'course.php' ?>
+
+    </div>
+
+</div>
