f2988d50399f3e389f31cbc206d6f44f7878f2d7
yygatech <yygatech@gmail.com>
2018-03-17 15:53:53 -0500

    Add components to course medal

--- course.php
+++ course.php
@@ -18,19 +18,71 @@
 <div class="modal" id="course">
     <div class="modal-dialog">
         <div class="modal-content">
+
             <div class="modal-header">
-                <h5 class="modal-title">Modal Title</h5>
+                <h5 class="modal-title">Food name</h5>
                 <button class="close" data-dismiss="modal">&times;</button>
             </div>
+
             <div class="modal-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus unde veniam harum magnam molestias dignissimos omnis architecto, quod, obcaecati dolorum debitis dolore porro qui, iusto quo accusantium voluptates pariatur illo.
+                <div class="container">
+                    <div class="row mb-4">
+                        <div class="col">
+                            <img src="assets/img/course/generaltsoschicken.jpeg" alt="food picture">
+                        </div>
+
+                        <div class="col">
+                            <div class="row mb-2">
+                                Today's special!
+                            </div>
+
+                            <div class="row mb-4">
+                                <strong>$</strong>
+                                <strong id="price">10.95</strong>
+                            </div>
+
+                            <div class="row">
+                                <div class="input-group col-9 m-auto">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-danger btn-number" type="button" data-type="minus" data-field="quant[2]">
+                                            <h4>-</h4>
+                                        </button>
+                                    </span>
+                                    <input type="text" name="quant[2]" class="form-control input-number text-center" value="1" min="1" max="10">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-success btn-number" type="button" data-type="plus" data-field="quant[2]">
+                                            <h4>+</h4>
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-primary btn-block">Add to bag</button>
+                        </div>
+
+                        <div class="col">
+                            <button class="btn btn-secondary btn-block" data-dismiss="modal">Cancel</button>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
             </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+
         </div>
     </div>
 </div>
 
-
+<script src="quantity.js"></script>
 
--- courseList.php
+++ courseList.php
@@ -15,3 +15,12 @@
     </div>
 
 </div>
+
+<!-- PAGINATION -->
+<nav>
+    <ul class="pagination justify-content-center">
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+    </ul>
+</nav>
