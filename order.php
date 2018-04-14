0fe90efb656ec7a8d525365cbe0b47d04bc9bf24
Laurence <hxl173830@utdallas.edu>
2018-04-14 16:14:23 -0500

    Update order.js/order.php/OderList.php/orderCtrl.php by Laurence

--- order.php
+++ order.php
@@ -1,42 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: henry
- * Date: 3/12/18
- * Time: 21:39
- */
-
-?>
-
-<?php session_start(); ?>
+<?php?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include 'head.php' ?>
     <title>Restaurant</title>
+    <script src="order.js"></script>
 </head>
 <body>
-<!-- Header(Navigation bar) -->
-<?php include 'header.php' ?>
+    <!-- Header(Navigation bar) -->
+    <?php include 'header.php' ?>
 
-<!-- Homepage Content -->
-<div class="container-fluid">
-    <div class="row">
-        <div id="index-view" class="col active">
-            <div class="container-fluid">
-                <div class="row">
-                    <?php include 'OrderList.php'?>
-                </div>
-                <div class="row">
-                    <?php include 'footer.php' ?>
+    <!-- Homepage Content -->
+    <div class="container-fluid">
+        <div class="row">
+            <div id="index-view" class="col active">
+                <div class="container-fluid">
+                    <div class="row">
+                        <?php include 'OrderList.php'?>
+                    </div>
+                    <div class="row">
+                        <?php include 'footer.php' ?>
+                    </div>
                 </div>
             </div>
+
+            <?php include 'cart.php' ?>
         </div>
-
-        <?php include 'cart.php' ?>
     </div>
-</div>
-
 </body>
 </html>
